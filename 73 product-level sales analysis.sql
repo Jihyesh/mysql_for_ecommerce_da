d8252a014f84c70447c3063dfeafefdbbcb3b2f8
Jihye <105202316+Jihyesh@users.noreply.github.com>
2025-01-12 14:38:39 +0900

    MySQL code for product analysis

---
+++ 73 product-level sales analysis.sql
@@ -0,0 +1,11 @@
+ SELECT
+	YEAR(created_at) AS yr,
+    MONTH(created_at) AS mo,
+    -- SUM(items_purchased) AS number_of_sales,
+    COUNT(DISTINCT order_id) AS number_of_sales,
+    SUM(price_usd) AS total_revenue,
+	SUM(price_usd - cogs_usd) AS total_margin
+    
+ FROM orders
+ WHERE created_at < '2023-01-04' -- date of request
+ GROUP BY YEAR(created_at), MONTH(created_at)
